a1c5b25e52ec4046a9458c5555a1a12b6ca8e021
HanSaloZu <conswintcomin@gmail.com>
2025-07-15 08:55:09 +0200

    Add Issue model

---
+++ models/Issue.py
@@ -0,0 +1,40 @@
+from .Base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from enum import Enum
+from sqlalchemy import Integer, DateTime, Text
+import datetime
+
+
+class StatusEnum(str, Enum):
+    open = "open"
+    closed = "closed"
+
+
+class SentimentEnum(str, Enum):
+    positive = "positive"
+    negative = "negative"
+    neutral = "neutral"
+    unknown = "unknown"
+
+
+class CategoryEnum(str, Enum):
+    technical = "техническая"
+    payment = "оплата"
+    other = "другое"
+
+
+class Issue(Base):
+    __tablename__ = "issues"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    text: Mapped[str] = mapped_column(Text, nullable=False)
+    status: Mapped[StatusEnum] = mapped_column(default=StatusEnum.open, nullable=False)
+    timestamp: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=datetime.datetime.now(datetime.timezone.utc),
+        nullable=False,
+    )
+    sentiment: Mapped[SentimentEnum] = mapped_column(nullable=False)
+    category: Mapped[CategoryEnum] = mapped_column(
+        default=CategoryEnum.other, nullable=False
+    )
